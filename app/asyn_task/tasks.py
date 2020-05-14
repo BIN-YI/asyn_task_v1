cf67fe6e5c87b6f1e7f7dbb177029b925b4db4fb
Dante <dabte_lin@akousist.com>
2020-05-14 15:17:18 +0800

    initial commit

--- app/asyn_task/tasks.py
+++ app/asyn_task/tasks.py
@@ -3,7 +3,7 @@ from ..recog import recog_func
 
 @celery.task(bind=True)
 def start_asyn_task(self, total):
-    #total
+    #tot
     recog_func.long_count_tasks(self, total)
     #函數結束，則回傳結束訊息
     return {'current': total,   otal': total, 'status': 'Task completed!', 'result': 'test success'}
